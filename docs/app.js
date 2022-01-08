06f0928a785c7d3fa592bc725f90d4b762a4643a
yotto <youdai0208@gmail.com>
2022-01-08 08:52:36 +0000

    path変更

--- docs/app.js
+++ docs/app.js
@@ -12,9 +12,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(__dirname + '/docs'));
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 module.exports = app;
